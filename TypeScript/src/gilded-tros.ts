ca01adfd2f188b4f14dbeaa654f1cc23f3287c7d
carlfremault <carlfremault@yahoo.com>
2026-03-25 15:08:42 +0100

    [Refactor - WIP] define constants, extract helper functions

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -1,63 +1,61 @@
-import {Item} from './item';
+import { Item } from "./item";
+
+// QUALITY
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+const LEGENDARY_QUALITY = 80;
+
+// ITEMS
+const LEGENDARY_ITEMS = ["B-DAWG Keychain"];
+const WELL_AGING_ITEMS = ["Good Wine"];
+const BACKSTAGE_PASSES = [
+  "Backstage passes for Re:Factor",
+  "Backstage passes for HAXX",
+];
+
+const isLegendaryItem = (item: Item): boolean =>
+  LEGENDARY_ITEMS.includes(item.name);
+const isWellAgingItem = (item: Item): boolean =>
+  WELL_AGING_ITEMS.includes(item.name);
+const isBackstagePass = (item: Item): boolean =>
+  BACKSTAGE_PASSES.includes(item.name);
+
+const hasExpired = (item: Item): boolean => item.sellIn < 0;
+
+const decreaseSellIn = (item: Item): Item => {
+  if (isLegendaryItem(item)) {
+    return item;
+  }
+  return { ...item, sellIn: item.sellIn - 1 };
+};
+
+const updateQuality = (item: Item): Item => {
+  if (isLegendaryItem(item)) {
+    return item;
+  }
+
+  if (hasExpired(item)) {
+    return {
+      ...item,
+      quality: Math.max(MIN_QUALITY, item.quality - 2),
+    };
+  }
+
+  return {
+    ...item,
+    quality: Math.max(MIN_QUALITY, item.quality - 1),
+  };
+};
 
 export class GildedTros {
+  constructor(public items: Array<Item>) {}
 
-    constructor(public items: Array<Item>) {
-
+  public updateQuality(): void {
+    for (let i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+      item = decreaseSellIn(item);
+      item = updateQuality(item);
+      this.items[i] = item;
     }
-
-    public updateQuality(): void {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Good Wine' && this.items[i].name != 'Backstage passes for Re:Factor'
-                && this.items[i].name != 'Backstage passes for HAXX') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'B-DAWG Keychain') {
-                        this.items[i].quality = this.items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1;
-
-                    if (this.items[i].name == 'Backstage passes for Re:Factor' || this.items[i].name == 'Backstage passes for HAXX') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
-                        }
-
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (this.items[i].name != 'B-DAWG Keychain') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Good Wine') {
-                    if (this.items[i].name != 'Backstage passes for Re:Factor' && this.items[i].name != 'Backstage passes for HAXX') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'B-DAWG Keychain') {
-                                this.items[i].quality = this.items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
-
+  }
 }
-
